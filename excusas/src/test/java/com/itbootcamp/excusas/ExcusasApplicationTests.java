77c18b09d4fc660693ef7f69076c512df90c2bcc
juan-aguirre-ml <aguirre.juan@mercadolibre.com>
2021-04-13 15:46:32 -0300

    Old pending commit

---
+++ excusas/src/test/java/com/itbootcamp/excusas/ExcusasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbootcamp.excusas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcusasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
